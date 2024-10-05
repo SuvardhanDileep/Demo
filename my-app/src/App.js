06395c362473bf2c24c0f9594cb0e01e55998e33
SuvardhanDileep <gsuvardhandileep@gmail.com>
2024-10-05 16:52:47 +0530

    Demo

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Suvardhan Dileep
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
